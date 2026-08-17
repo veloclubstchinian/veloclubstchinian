c8993340c6d4e5e53d94badeba260f3718669cc3
veloclubstchinian <veloclubstchinian@gmail.com>
2026-08-17 10:20:21 +0200

    Add files via upload

--- data/evenements.js
+++ data/evenements.js
@@ -9,7 +9,7 @@ const evenementsData = [
     "nom": "XC",
     "date": "DIMANCHE 27 SEPTEMBRE 2026",
     "photos": [
-      "assets/images/uploads/XCO 2026.PNG"
+      "assets/images/uploads/XCO 2026_1.JPG"
     ]
   },
   {
